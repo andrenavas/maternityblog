442caf5a047e8e3a1346087f15acf29a353e66c5
andrenavas <andreynanavas@gmail.com>
2023-12-14 12:10:46 -0500

    Initial configuration for frontend and backend

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('¡Hola desde el backend!');
+});
+
+app.listen(port, () => {
+  console.log(`El servidor está escuchando en el puerto ${port}`);
+});
